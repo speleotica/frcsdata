f29d2d0e1cefd524c15f15ceaad67592c8998d16
Andy Edwards <jedwards@fastmail.com>
2019-11-24 15:27:56 -0600

    build: add publishConfig

--- node/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const parseFrcsSurveyFile: (
-  file: string
-) => Promise<import('..').FrcsSurveyFile>
-export declare const parseFrcsPlotFile: (
-  file: string
-) => Promise<import('..').FrcsPlotFile>
-export declare const parseFrcsTripSummaryFile: (
-  file: string
-) => Promise<import('..').FrcsTripSummaryFile>
